96c9177ba08941e031f3f9bfd25b3cef7a9ebb4e
Tyler Coleman <tylerblake.tc@gmail.com>
2017-12-05 21:19:18 -0600

    Finished the How to play screen

--- src/Tetris.java
+++ src/Tetris.java
@@ -1,5 +1,8 @@
 import java.awt.event.*;
 import javax.swing.*;
+
+import javafx.scene.image.Image;
+
 import java.util.*;
 import java.util.Timer;
 
@@ -9,6 +12,7 @@ public class Tetris {
 	private static Board gameBoard;
 	//private static home splash;
 	private static JPanel splashScreen;
+	private static JPanel infoScreen;
 	private static Timer timer;
 	
 	private static boolean isFalling;
@@ -44,11 +48,22 @@ public class Tetris {
 	}
     
 	public static void createSplash() {
+		frame.getContentPane().removeAll();
+		
 		splashScreen = new JPanel();
 		
     	JButton start = new JButton("Start Game");
     	JButton info = new JButton("How to Play");
     	JButton credits = new JButton("Credits");
+    	
+    	JLabel heading = new JLabel("Tetris");
+    	heading.setBounds(65, 25, 250, 250);
+    	heading.setFont (heading.getFont ().deriveFont (64.0f));
+    	splashScreen.add(heading);
+    	
+//    	JLabel heading = new JLabel(new ImageIcon("/pics/tetlog.jpg"));
+//    	heading.setBounds(65, 25, 250, 250);
+//    	splashScreen.add(heading);
     	
     	splashScreen.setLayout(null);
     	
@@ -65,7 +80,7 @@ public class Tetris {
     	info.setBounds(95,260,100,50);
     	info.addActionListener(new ActionListener() {
     	    public void actionPerformed(ActionEvent evt) {
-    	    	//show info screen
+    	    	createInfoScreen();
     	    }
     	});
     	splashScreen.add(info);
@@ -79,8 +94,50 @@ public class Tetris {
     	splashScreen.add(credits);
     	
     	frame.add(splashScreen);
-    	//frame.revalidate();
-        //frame.repaint();
+    	frame.revalidate();
+        frame.repaint();
+	}
+	
+	public static void createInfoScreen() {
+		//new info screen
+		infoScreen = new JPanel();
+		//remove the current screen and revalidate
+		frame.getContentPane().removeAll();
+		frame.add(infoScreen);
+        frame.revalidate();
+        frame.repaint();
+        
+		infoScreen.setLayout(null);
+		
+		JButton back = new JButton("Back");
+		JLabel title = new JLabel("How to Play");
+		JLabel control = new JLabel("Controls:");
+		JLabel controls = new JLabel("<html><p><ul><li>Up Arrow & SpaceBar:<br></i> instantly moves a piece to the bottom.<br><br>"
+				+ "<li>Down Arrow:<br> Accelerates the piece downward.<br><br>"
+				+ "<li>Left Arrow:<br> Moves the piece to the left.<br><br>"
+				+ "<li>Right Arrow:<br> Moves the piece to the right.<br><br>"
+				+ "<li>Z:<br> Rotates the piece counter-clockwise.<br><br>"
+				+ "<li>X:<br> Rotates the piece clockwise.</ul></p></html>");
+		
+    	title.setBounds(70, 25, 150, 25);
+    	title.setFont (title.getFont ().deriveFont (24.0f));
+    	infoScreen.add(title);
+    	
+    	control.setBounds(0, 30, 300, 150);
+    	control.setFont (title.getFont ().deriveFont (20.0f));
+    	infoScreen.add(control);
+    	controls.setBounds(0, 100, 300, 300);
+    	controls.setFont (title.getFont ().deriveFont (12.0f));
+    	infoScreen.add(controls);
+    	
+    	back.setBounds(100, 400, 75, 30);
+    	back.addActionListener(new ActionListener() {
+    	    public void actionPerformed(ActionEvent evt) {
+    	    	createSplash();
+    	    }
+    	});
+    	infoScreen.add(back);
+    	
 	}
 	
 	//Called to start our tetris game
@@ -88,7 +145,7 @@ public class Tetris {
 		//remove our splash screen
 		//System.out.println("removing all panels");
 		frame.getContentPane().removeAll();
-		splashScreen.setVisible(false);
+		//splashScreen.setVisible(false);
 		//System.out.println("removed all panels");
 		
 		//add game board
